f7dd08faebd93359f1f90e1a28a028fab463ad96
Patrik Johnson <patrik@zebros.fi>
2015-04-13 00:26:08 +0300

    Dropdown menu selections toggle displayed screen sizes, fix assignment logic in template for lock variable which now enables or disables changing selection

--- src/app/components/header/HeaderCtrl.js
+++ src/app/components/header/HeaderCtrl.js
@@ -8,9 +8,15 @@
     $scope.active = active;
     $scope.lock = false;
 
-    $scope.mobiles = screenSizes.mobile;
-    $scope.tablets = screenSizes.tablet;
-    $scope.desktops = screenSizes.desktop;
+    $scope.$watch('active.screens', function() {
+      $scope.activeNames = $scope.active.screens.map(function(elem) { return elem.name; });
+    }, true);
+
+    $scope.devices = [
+      { type: 'Mobile',  screens: screenSizes.mobile  },
+      { type: 'Tablet',  screens: screenSizes.tablet  },
+      { type: 'Desktop', screens: screenSizes.desktop }
+    ];
 
     $scope.loadPage = function(url) {
       console.log('loadPage ' + url);
@@ -22,6 +28,11 @@
           screens   : []
         });
       }
+    };
+
+    $scope.toggleScreen = function(screenName) {
+      if (!$scope.lock)
+        active.toggle(screenName);
     };
 
     $scope.updatePage = function() {
--- src/app/model/screenSizes.js
+++ src/app/model/screenSizes.js
@@ -8,7 +8,7 @@
         { name: 'Android Small', width: 320, height: 426 },
         { name: 'Android Normal', width: 320, height: 470 },
         { name: 'Android Large', width: 480, height: 640 },
-        { name: 'Android Extra-Large', width: 720, height: 960 },
+        { name: 'Android XL', width: 720, height: 960 },
         { name: 'iPhone 3GS', width: 320, height: 480 },
         { name: 'iPhone 4S', width: 640, height: 960 },
         { name: 'iPhone 5', width: 640, height: 1136 }
@@ -18,7 +18,7 @@
         { name: 'iPad', width: 1536, height: 2048 }
       ],
       desktop: [
-        { name: '1024x768', width: 600, height: 768 },
+        { name: '1024x768', width: 1024, height: 768 },
         { name: '1280x800', width: 1280, height: 800 },
         { name: '1280 x 1024', width: 1280, height: 1024 },
         { name: '1366 x 768', width: 1366, height: 768 },
