602072dc2ac586daa67290a9fcb412902b4c9db7
Patrik Johnson <patrik@zebros.fi>
2015-04-12 17:13:21 +0300

    Add moment.js filter to history visit timestamps for better readability

---
+++ src/app/shared/filters.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular.module('exposure').service('moment', [function() {
+    return moment;
+  }]);
+
+  angular.module('exposure').filter('fromNow', ['moment', function(moment) {
+    return function(dateString) {
+      return moment(dateString).fromNow();
+    };
+  }]);
+})();
