af00a59edfbff068f06f642c532b19409163220a
Patrik Johnson <patrik@zebros.fi>
2015-04-13 22:57:21 +0300

    Add "header" to iframe with selected screen name, height, width and a toggle button to change between portrait and landscape modes

--- src/app/model/screenSizes.js
+++ src/app/model/screenSizes.js
@@ -5,24 +5,24 @@
     // portrait orientation
     return {
       mobile: [
-        { name: 'Android Small', width: 320, height: 426 },
-        { name: 'Android Normal', width: 320, height: 470 },
-        { name: 'Android Large', width: 480, height: 640 },
-        { name: 'Android XL', width: 720, height: 960 },
-        { name: 'iPhone 3GS', width: 320, height: 480 },
-        { name: 'iPhone 4S', width: 640, height: 960 },
-        { name: 'iPhone 5', width: 640, height: 1136 }
+        { name: 'Android Small',  width: 320, height: 426,  portrait: true },
+        { name: 'Android Normal', width: 320, height: 470,  portrait: true },
+        { name: 'Android Large',  width: 480, height: 640,  portrait: true },
+        { name: 'Android XL',     width: 720, height: 960,  portrait: true },
+        { name: 'iPhone 3GS',     width: 320, height: 480,  portrait: true },
+        { name: 'iPhone 4S',      width: 640, height: 960,  portrait: true },
+        { name: 'iPhone 5',       width: 640, height: 1136, portrait: true }
       ],
       tablet: [
-        { name: 'iPad Mini', width: 768, height: 1024 },
-        { name: 'iPad', width: 1536, height: 2048 }
+        { name: 'iPad Mini', width: 768,  height: 1024, portrait: true },
+        { name: 'iPad',      width: 1536, height: 2048, portrait: true }
       ],
       desktop: [
-        { name: '1024x768', width: 1024, height: 768 },
-        { name: '1280x800', width: 1280, height: 800 },
-        { name: '1280 x 1024', width: 1280, height: 1024 },
-        { name: '1366 x 768', width: 1366, height: 768 },
-        { name: '1080p', width: 1920, height: 1080 }
+        { name: '1024x768',  width: 1024, height: 768  },
+        { name: '1280x800',  width: 1280, height: 800  },
+        { name: '1280x1024', width: 1280, height: 1024 },
+        { name: '1366x768',  width: 1366, height: 768  },
+        { name: '1080p',     width: 1920, height: 1080 }
       ]
     };
   }]);
